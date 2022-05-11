0b055dc30abfd6bd8f9d9e3e293217490153d113
Claudio Contreras-Aburto <clcontreras@uv.mx>
2022-05-11 17:46:02 -0500

    Modified console output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello" << std::endl;
+    std::cout << "World" << std::endl;
+    std::cout << "!" << std::endl;
     return 0;
 }
